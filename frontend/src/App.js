d5ccadbd1615f6f3b0b856ad6601ad56d9fed9db
Ching-Ting Lu <chingtinglu@Ching-Tings-MacBook-Pro.local>
2021-11-22 14:39:06 -0800

    create account page w/o css + move code to Lists.js + basic routes setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,61 +1,17 @@
 import React, { useState } from "react";
-import { Button, InputGroup, FormControl } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import CreateAccount from "./login/CreateAccount";
+import Lists from "./Lists";
 
 function App() {
-  const [items, setItems] = useState([]);
-  const [newItem, setNewItem] = useState("");
-
-  const handleChange = (e) => {
-    setNewItem(e.target.value);
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (newItem) {
-      setItems([...items, newItem]);
-      setNewItem("");
-    }
-    e.target.reset();
-  }
-
-  const removeItem = (index) => {
-
-  }
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>To-do List</p>
-      </header>
-      <div className="list">
-        <div className='list-items'>
-        {/* add remove itme button */}
-          {
-            items.map((item, index) => {
-              return (
-                <div key={index}>
-                  <p>{item}</p>
-                </div>
-              )
-            })
-          }
-        </div>
-
-        <form onSubmit={handleSubmit}>
-          <InputGroup className="mb-3">
-            <FormControl
-              placeholder="Type here"
-              aria-label="new-item"
-              onChange={handleChange}
-            />
-            <Button variant="primary" type="submit">
-              Add to list
-            </Button>
-          </InputGroup>
-        </form>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Lists/>}/>
+        <Route exact path="/createaccount" element={<CreateAccount/>}/>  
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/login/CreateAccount.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import '../index.css';
+
+const CreateAccount = () => {
+    useEffect(() => {
+        document.title = 'Sign Up';
+    });
+    
+    const [username, setName] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const user = {
+        username: username,
+        password: password,
+    }
+
+    const navigate = useNavigate();
+    const handleButton = (e) => {
+                e.preventDefault();
+        // redirect to list or somewhere you want
+        // navigate.push('/');
+    }
+
+    return(   
+        <div className="create">
+            <div className="userform">
+            <h2 className="pagename">Create account</h2>
+            <form>
+                <input
+                    type="text"
+                    required
+                    value={ username }
+                    onChange={ (e) => setName(e.target.value) }
+                    placeholder="Create Username"
+                />
+                <input
+                    type="text"
+                    required
+                    value={ password }
+                    onChange={ (e) => setPassword(e.target.value) }
+                    placeholder="Create Password"
+                />
+                <button onClick={handleButton}> Create Account </button>
+            </form>
+            </div>
+            <div className="redirect">
+                <label className="message">Already have an account?</label>
+                <div className="accountlinks">
+                    {/* <Link to="/login" className="link">Log in</Link> */}
+                </div>
+            </div>
+        </div>
+    );
+    
+}
+export default CreateAccount;
