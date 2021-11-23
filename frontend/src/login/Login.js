86530958d2725283b429fa92c95110acb18c514d
Shania Hao <52763254+shaniahao@users.noreply.github.com>
2021-11-23 15:47:14 -0800

    Update Login.js

--- frontend/src/login/Login.js
+++ frontend/src/login/Login.js
@@ -1,6 +1,19 @@
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import "../index.css";
+
 const Login = () => {
+  useEffect(() => {
+    document.title = "Sign In";
+  });
 
+  const [username, setName] = useState("");
+  const [password, setPassword] = useState("");
 
-}
-
+  const user = {
+    username: username,
+    password: password,
+  };
+};
 export default Login;
