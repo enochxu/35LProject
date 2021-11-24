45118bea8782254d1b4b76d98981f8d0a2be4e8f
kmachnickiwork49 <37753492+kmachnickiwork49@users.noreply.github.com>
2021-11-24 00:33:34 -0800

    Changes the internals of Lists.js from string items to object items and uses these objects to try to display time of note written

--- frontend/src/Lists.js
+++ frontend/src/Lists.js
@@ -2,22 +2,31 @@ import React, { useEffect, useState } from 'react';
 import { Button, InputGroup, FormControl } from "react-bootstrap";
 import './index.css';
 
+var TodoLine = function(date, info) {
+  this.date = date;
+  this.info = info;
+}
+
+TodoLine.prototype.getString = function() {
+  return (this.date + " " + this.info);
+}
+
 const Lists = () => {
     useEffect(() => {
         document.title = 'Lists';
     });
     const [items, setItems] = useState([]);
-  const [newItem, setNewItem] = useState("");
+  const [newItem, setNewItem] = useState(new TodoLine(Date(), ""));
 
   const handleChange = (e) => {
-    setNewItem(e.target.value);
+    setNewItem(new TodoLine(Date(), e.target.value));
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (newItem) {
       setItems([...items, newItem]);
-      setNewItem("");
+      setNewItem(new TodoLine(Date(), ""));
     }
     e.target.reset();
   }
@@ -39,7 +48,10 @@ const Lists = () => {
             items.map((item, index) => {
               return (
                 <div key={index}>
-                  <p>{item}</p>
+                  <p>
+		    {item.info}
+                    <small><details>{item.date}</details></small>
+                  </p>
                 </div>
               )
             })
