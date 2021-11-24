65785d51fa6bb1ed7512cd4afea9a19915ed8289
Shania Hao <52763254+shaniahao@users.noreply.github.com>
2021-11-23 18:24:24 -0800

    Update Login.js

--- frontend/src/login/Login.js
+++ frontend/src/login/Login.js
@@ -15,5 +15,65 @@ const Login = () => {
     username: username,
     password: password,
   };
+
+  const navigate = useNavigate();
+  const handleButton = (e) => {
+    e.preventDefault();
+    axios({
+      method: "post",
+      data: {
+        username: username,
+        password: password,
+      },
+      url: `http://localhost:5000/verifyaccount`,
+      withCredentials: true,
+    }).then(
+      (res) => {
+        console.log(res)
+        if (res == "true") {
+          // send user to login page 
+        }
+        else {
+          // display error message
+        }
+      // navigate.push('/');
+    }).catch((err) => {
+      console.log(err);
+      // create error message / banner?
+    });
+    // redirect to list or somewhere you want
+    // navigate.push('/');
+  };
+
+  return (
+    <div className="create">
+      <div className="userform">
+        <h2 className="pagename">Login</h2>
+        <form>
+          <input
+            type="text"
+            required
+            value={username}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Username"
+          />
+          <input
+            type="text"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+          <button onClick={handleButton}> Login </button>
+        </form>
+      </div>
+      <div className="redirect">
+        <label className="message">Create an account</label>
+        <div className="accountlinks">
+          {/* <Link to="/createAccount" className="link">Create Account</Link> */}
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Login;
